ee973ab31f089ee302a8e209ae03f4fe0636dcdb
jiyoony <65219386+jiyoony@users.noreply.github.com>
2023-11-22 21:47:38 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.28 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,17 @@
+def solution(survey, choices):
+    point = {'R': 0,'T': 0, 'C': 0,'F': 0, 'J': 0,'M': 0, 'A': 0, 'N':0}
+    mbti = [('R','T'),('C','F'),('J','M'),('A','N')]
+    answer = ''
+    for q, p in zip(survey,choices):
+        if p >= 5:
+            point[q[1]] += p-4
+        elif p <= 3:
+            point[q[0]] += abs(p-4)
+    
+    for a,b in mbti:
+        if point[a] >= point[b]:
+            answer += a
+        else:
+            answer += b
+    print(point)
+    return answer
