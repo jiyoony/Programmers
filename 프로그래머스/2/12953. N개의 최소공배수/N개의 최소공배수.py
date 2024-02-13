a40bfa60eb447038d078411f5a9630139d72114c
jiyoony <65219386+jiyoony@users.noreply.github.com>
2024-02-14 00:30:53 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,11 @@
+import math
+def lcm(a,b):
+    return int(a * b / math.gcd(a,b))
+
+def solution(arr):
+    answer = arr[0]
+    for i in arr[1:]:
+        answer = lcm(answer,i)
+    return answer
+
+    
