dbc4773ee4b3baa56b16a7e9914613c87c07af2b
jiyoony <65219386+jiyoony@users.noreply.github.com>
2024-10-24 13:13:37 +0900

    [level 1] Title: 문자열 나누기, Time: 2.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,19 +1,22 @@
+from collections import deque
 def solution(s):
     answer = 0
-    while s:
-        x = s[0]
-        x_cnt, non_cnt = 0, 0
-        for j in range(len(s)):
-            if s[j] == x:
+    q = deque(s)
+    while q:
+        x_cnt, non_cnt = 1,0
+        x = q.popleft()
+        while q:
+            nxt = q.popleft()
+            if x == nxt:
                 x_cnt += 1
             else:
                 non_cnt += 1
+
             if x_cnt == non_cnt:
                 answer += 1
-                s = s[j+1:]
                 break
         if x_cnt != non_cnt:
             answer += 1
-            break
+            
         
     return answer
